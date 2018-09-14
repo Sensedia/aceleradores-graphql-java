df0a0b18cd3574de64a10f990d020f8bda9042c6
Leandro Vaneli <leandro.vaneli@sensedia.com>
2018-09-14 14:47:54 -0300

    Incluindo models e resolver genericos (ainda sem implementacao)

---
+++ src/main/java/br/com/sensedia/aceleradores/inputs/CoordinateInput.java
@@ -0,0 +1,25 @@
+package br.com.sensedia.aceleradores.inputs;
+
+public class CoordinateInput {
+  
+  private Integer lon;
+
+  private Integer lat;
+
+  public Integer getLon() {
+    return lon;
+  }
+
+  public void setLon(Integer lon) {
+    this.lon = lon;
+  }
+
+  public Integer getLat() {
+    return lat;
+  }
+
+  public void setLat(Integer lat) {
+    this.lat = lat;
+  }
+
+}
---
+++ src/main/java/br/com/sensedia/aceleradores/inputs/CountryInput.java
@@ -0,0 +1,35 @@
+package br.com.sensedia.aceleradores.inputs;
+
+public class CountryInput {
+  
+  private String name;
+
+  private String fullName;
+
+  private Integer alphaCode;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public Integer getAlphaCode() {
+    return alphaCode;
+  }
+
+  public void setAlphaCode(Integer alphaCode) {
+    this.alphaCode = alphaCode;
+  }
+
+}
