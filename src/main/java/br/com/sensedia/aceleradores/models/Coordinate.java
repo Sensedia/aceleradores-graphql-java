3f9df74601abc8112a622b5909164112dcc6c701
Leandro Vaneli <leandro.vaneli@sensedia.com>
2018-09-14 11:27:08 -0300

    Incluindo models

--- src/main/java/br/com/sensedia/aceleradores/models/Coordinate.java
+++ src/main/java/br/com/sensedia/aceleradores/models/Coordinate.java
@@ -1,4 +1,24 @@
 package br.com.sensedia.aceleradores.models;
 
 public class Coordinate {
+  
+  private Integer lon;
+
+  private Integer lat;
+
+  public Integer getLon() {
+    return lon;
+  }
+
+  public void setLon(Integer lon) {
+    this.lon = lon;
+  }
+
+  public Integer getLat() {
+    return lat;
+  }
+
+  public void setLat(Integer lat) {
+    this.lat = lat;
+  }
 }
