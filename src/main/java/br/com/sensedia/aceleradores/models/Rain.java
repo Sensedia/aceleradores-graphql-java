ab0ace4af5522cc87e4954096fba1a805e7884d7
Leandro Vaneli <leandro.vaneli@sensedia.com>
2018-09-14 11:47:49 -0300

    incluindo models

--- src/main/java/br/com/sensedia/aceleradores/models/Rain.java
+++ src/main/java/br/com/sensedia/aceleradores/models/Rain.java
@@ -1,4 +1,15 @@
 package br.com.sensedia.aceleradores.models;
 
 public class Rain {
+  
+  private Integer threeH;
+
+  public Integer getThreeH() {
+    return threeH;
+  }
+
+  public void setThreeH(Integer threeH) {
+    this.threeH = threeH;
+  }
+  
 }
