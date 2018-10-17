3ef7710cc37ccab29a2296dd36fd124b6594051c
Wagner Porto <wagner.porto@sensedia.com>
2018-10-17 16:32:07 -0300

    Add Consulta de Weather

--- src/main/java/br/com/sensedia/aceleradores/infraestructure/weather/client/WeatherRestClient.java
+++ src/main/java/br/com/sensedia/aceleradores/infraestructure/weather/client/WeatherRestClient.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Service
 public class WeatherRestClient {
 
